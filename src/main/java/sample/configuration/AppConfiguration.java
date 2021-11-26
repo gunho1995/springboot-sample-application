037bd97f1064578e6c9670b85af4c97990caac02
Crispy <gunho19945@gmail.com>
2021-11-26 23:48:15 +0900

    [개발] Configuration Properties

--- src/main/java/sample/configuration/AppConfiguration.java
+++ src/main/java/sample/configuration/AppConfiguration.java
@@ -7,9 +7,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 @EnableConfigurationProperties(value = AppConfiguration.class)
 @EnableAutoConfiguration
 public class AppConfiguration {
+    public class AppConfinguration{
 
+    }
+    public class ListBootStrapSever{
+        List<String> bootStrapSevers = new List<String>();
+    }
 }
