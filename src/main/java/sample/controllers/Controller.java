a7d96beafa375ae77505b9560bde4cc0404bd27a
Crispy <gunho19945@gmail.com>
2021-12-01 21:46:25 +0900

    [수정] KafkaService 참조

--- src/main/java/sample/controllers/Controller.java
+++ src/main/java/sample/controllers/Controller.java
@@ -7,15 +7,31 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import sample.services.KafkaService;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @RequestMapping("/kafka/clusters")
 @AllArgsConstructor
 public class Controller {
+    private KafkaService kafkaService;
+
     @ApiOperation(value = "카프카 토픽 생성")
     @PostMapping("/{clusterId}/topics")
     public void createTopic(@PathVariable String clusterId,
                               @RequestParam String topicName,@RequestParam int numPartitions,
-                              @RequestParam short replicationFactor){
-        //kafkaService.createTopic(topicName, numPartitions, replicationFactor);
+                              @RequestParam short replicationFactor) throws ExecutionException, InterruptedException {
+        kafkaService.createTopic(topicName, numPartitions, replicationFactor);
+    }
+
+    @ApiOperation(value = "카프카 목록 조회")
+    @GetMapping("/{clusterId}/topics")
+    public void listTopics(@PathVariable String clusterId) throws ExecutionException, InterruptedException {
+        kafkaService.listTopic();
+    }
+
+    @ApiOperation(value = "카프카 토픽 조회")
+    @GetMapping("/{clusterId}/topics/{topicName}")
+    public void describeTopic(){
+        
     }
 }
